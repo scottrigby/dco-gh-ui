ace21883af31f8a9222c1f40f5e1b48f20fab38d
Scott Rigby <scott@r6by.com>
2018-09-19 15:45:28 -0400

    Comment why we use content scripts instead of background scripts
    Signed-off-by: Scott Rigby <scott@r6by.com>

--- content.js
+++ content.js
@@ -1,3 +1,8 @@
+// Note we use a content script instead of a background script because:
+// > Background scripts can access all the WebExtension JavaScript APIs, but
+// they can't directly access the content of web pages. So if your extension
+// needs to do that, you need content scripts.
+// See https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Content_scripts
 chrome.storage.local.get({
     signoff: ''
 }, function (items) {
